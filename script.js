f17959cd06633aa3dcd339bc20b228f91ec20c64
Zainab <ajumobizainab@gmail.com>
2022-03-28 17:04:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// date in the footer
+function currentYear() {
+    const date = new Date();
+    const year = `${date.getFullYear()}`;
+    return year;
+}
+document.getElementById("year").innerHTML = currentYear();
+
+
+// show/hide mobile nav
+let toggleIcon = document.getElementById("icon");
+let mobileNav = document.querySelector('.mobile-nav');
+
+function toggleNav(){
+    const style = mobileNav.style.display === "flex" ? "none" : "flex";
+    mobileNav.style.display = style;
+    console.log(mobileNav.style.display);
+    toggleIcon.classList.toggle('bi-list');
+    toggleIcon.classList.toggle('bi-x');
+};
+
+// toggleIcon.addEventListener('click', toggleNav());
+
